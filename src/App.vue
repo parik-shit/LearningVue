d7497bda19cce2ee7cc86254eb7214ab4d7d9f5f
parik-shit <62713994+parik-shit@users.noreply.github.com>
2023-11-18 02:28:39 +0530

    "learned to pass props the hard way"

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,33 @@
 <template>
-  <h1> {{title}}</h1>
-  <input type = 'text' ref="name"> 
-  <button @click = "handleClick">click me</button>
+  <h1>{{ title }}</h1>
+  
+  <div v-if = "showModal">
+    <Modal :header="header" :text="text" theme="sale" @close="toggleModal" />
+  </div>
+  <button @click = "toggleModal" >Show modal</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal'
 
 export default {
   name: 'App',
-  data(){
+  components: { Modal }, 
+  data() {
     return {
-      title: 'My First Vue app :)'
+      title: 'My First Vue App!',
+      header: '10% off on Indri!' ,
+      text: 'Grab it before anyone else!',
+      showModal: false
+    }
+  },
+  methods: {
+    toggleModal (){
+      this.showModal = !this.showModal;
 
     }
   }
-  ,
-  methods: {
-    
-      handleClick(){
-        console.log(this.$refs.name)
-        this.$refs.name.classList.add('active')
-        this.$refs.name.focus
-      }
-    
-
   }
-}
 </script>
 
 <style>
@@ -37,5 +38,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
