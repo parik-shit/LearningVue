f45e1e20441a5dc32ad41f87e131c4c152b880e8
parik-shit <62713994+parik-shit@users.noreply.github.com>
2023-11-19 00:52:45 +0530

    learned to reuse the component (made modal 2)

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,59 @@
-//learning about slots - they are used to decrease the use of props and make the working of components more dynamic
+//learning about slots - they are used to decrease the use of props and make the
+working of components more dynamic
+
+//Insted of passing the the data inside the Modal div to the Modal
+      componenet,we are doing it here and send the data as a slot in the
+      modal component
 <template>
   <h1>{{ title }}</h1>
-  
-  <div v-if = "showModal">
-    <Modal :header="header" :text="text" theme="sale" @close="toggleModal" />
+
+  <div v-if="showModal">
+    <Modal theme="sale" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">Sign up</a>
+      </template>
+      <h1>10% off on Indri!</h1>
+      <p>Grab your bottle today!!</p>
+    </Modal>
   </div>
-  <button @click = "toggleModal" >Show modal</button>
+  <br>
+  <div v-if = "showModal2">
+    <Modal theme = "sale" @close = "toggleModal2">
+      <template v-slot: second>
+        <h1>20% off on Royal Stag!</h1>
+        
+      </template>
+    </Modal>
+  </div>
+  <button @click="toggleModal">Show modal</button>
+  <button @click = "toggleModal2">Show Modal 2</button>
 </template>
 
 <script>
-import Modal from './components/Modal'
+import Modal from "./components/Modal";
 
 export default {
-  name: 'App',
-  components: { Modal }, 
+  name: "App",
+  components: { Modal },
   data() {
     return {
-      title: 'My First Vue App!',
-      header: '10% off on Indri!' ,
-      text: 'Grab it before anyone else!',
-      showModal: false
-    }
+      title: "My First Vue App!",
+      header: "10% off on Indri!",
+      text: "Grab it before anyone else!",
+      showModal: false,
+      showModal2: false
+    };
   },
   methods: {
-    toggleModal (){
+    toggleModal() {
       this.showModal = !this.showModal;
+    },
+    toggleModal2() {
+      this.showModal2 = !this.showModal2
 
     }
-  }
-  }
+  },
+};
 </script>
 
 <style>
@@ -40,6 +65,7 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
 h1 {
   border-bottom: 1px solid #ddd;
   display: inline-block;
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,53 +1,56 @@
 <template>
-  <div class="backdrop" @click.self ="closeModal">
+  <div class="backdrop" @click.self="closeModal">
     <div class="modal" :class="{ sale: theme === 'sale' }">
-      <h1>{{ header }}</h1>
-      <p>{{ text }}</p>
+      <slot name = "second"></slot>
+      <slot></slot>
+      <div class="actions">
+        <slot name="links"></slot>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['header', 'text', 'theme'],
+  props: ["theme"],
   methods: {
     closeModal() {
-      this.$emit('close')
-    }
-  }
-}
+      this.$emit("close");
+    },
+  },
+};
 </script>
 
 <style>
-  .modal {
-    width: 400px;
-    padding: 20px;
-    margin: 200px auto;
-    background: white;
-    border-radius: 10px;
-  }
-  .backdrop {
-    top: 0;
-    position: fixed;
-    background: rgba(0,0,0,0.5);
-    width: 100%;
-    height: 100%;
-  }
-  .modal h1 {
-    color: #03cfb4;
-    border: none;
-    padding: 0;
-  }
-  .modal p {
-    font-style: normal;
-  }
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 200px auto;
+  background: white;
+  border-radius: 10px;
+}
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+.modal h1 {
+  color: #03cfb4;
+  border: none;
+  padding: 0;
+}
+.modal p {
+  font-style: normal;
+}
 
-  /* sale styles */
-  .sale {
-    background: crimson;
-    color: white;
-  }
-  .sale h1 {
-    color: white;
-  }
+/* sale styles */
+.sale {
+  background: crimson;
+  color: white;
+}
+.sale h1 {
+  color: white;
+}
 </style>
